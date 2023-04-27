8d55cbe38bc304efe9c1d8c0f45c8fafa948bb11
wbryon <tamirbudaev.habibula@gmail.com>
2023-04-27 12:57:18 +0300

    feat: добавлены аннотации Builder, AllArgsConstructor

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -1,10 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@Builder
+@AllArgsConstructor
 public class Review {
     private int reviewId;
     private String content;
