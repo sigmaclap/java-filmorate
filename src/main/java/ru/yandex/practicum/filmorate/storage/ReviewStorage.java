c02666cfa579670ada650f829df97c9275ca968b
wbryon <tamirbudaev.habibula@gmail.com>
2023-04-27 12:34:07 +0300

    feat: добавлен интерфейс ReviewStorage в пакет storage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/ReviewStorage.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.util.List;
+
+public interface ReviewStorage {
+    Review createReview(Review review);
+    Review updateReview(Review review);
+    boolean deleteReview(Integer reviewId);
+    Review getReviewById(Integer reviewId);
+    List<Review> getReviewsByFilmId(Integer filmId);
+}
